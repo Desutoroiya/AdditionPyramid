2dbd55359e73fdd3cb66a07803c8bb5973f0e5a3
erasmia <erasmia_10@hotmail.com>
2015-11-10 19:26:55 +0100

    GREEN
    Create Pyramid (decide the size of the arrays pyramid & userArray) -
    test success

--- src/TheGame/PyramidGame.java
+++ src/TheGame/PyramidGame.java
@@ -1,15 +1,21 @@
 package TheGame;
 
+import static org.junit.Assert.fail;
+
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import org.junit.Test;
 
 public class PyramidGame {
 
 	private int difficulty;
+	int[] pyramid;
+	boolean[] userArray;
 	public int randomNumbers;
 
 	Scanner scanner = new Scanner(System.in);
-	
+
 	public PyramidGame() {
 		int input = 0;
 		do {
@@ -25,19 +31,28 @@ public class PyramidGame {
 		} while (input < 1 || input > 3);
 		difficulty = input;
 	}
-	
-	public void createPyramid() {		
+
+	public void createPyramid() {
+		int size = 0;
+		if (difficulty == 1)
+			size = 4;
+		else if (difficulty == 2)
+			size = 8;
+		else
+			size = 16;
+		pyramid = new int[size];
+		userArray = new boolean[size];
 	}
-	
+
 	public int getDifficulty() {
 		return difficulty;
 	}
-	
+
 	public int[] getPyramid() {
-		return null;
+		return pyramid;
 	}
 
 	public boolean[] getUserArray() {
-		return null;
+		return userArray;
 	}
 }
