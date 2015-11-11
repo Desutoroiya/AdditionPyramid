8b3189bb34d34859badc9603f1fa0f8840b96020
Ivar de Lange <ivardelange2003@hotmail.com>
2015-11-11 20:45:00 +0100

    RED
    numberTest, makes the array for the user true if the numbers are in it.

--- src/Testing/createRandomNumbersTest.java
+++ src/Testing/createRandomNumbersTest.java
@@ -14,17 +14,17 @@ public class createRandomNumbersTest {
 	@Test
 	public void testRandomNumbers() {
 		PyramidGame game = new PyramidGame();
-		game.createPyramid();
 		game.createRandomNumbers();
 		if (game.getDifficulty() == 1) {
 			int size = (int) Math.pow(2, game.getDifficulty() + 1);
 			int[] pyramid = game.getPyramid();
+			boolean[] userArray = game.getUserArray();
 			for (int i = 0; i < size; i++)
-				if (pyramid[i] < 1 || pyramid[i] > 50) {
+				if (pyramid[i] < 1 || pyramid[i] > 50 || !userArray[i]) {
 					fail("No numbers created in array");
 				}
 			for (int i = size; i < pyramid.length; i++)
-				if (pyramid[i] != 0) {
+				if (pyramid[i] != 0 || userArray[i]) {
 					fail("Rest of array should be zero!");
 				}
 		}
