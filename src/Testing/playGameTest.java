aa9895353dfc061e9563bf69604c9b1acd23234d
erasmia <erasmia_10@hotmail.com>
2015-11-11 15:21:05 +0100

    RED
    Red test : setInputCell("1 2 3")

--- src/Testing/playGameTest.java
+++ src/Testing/playGameTest.java
@@ -37,4 +37,14 @@ public class playGameTest {
 		if (game.setInputCell("1 input"))
 			fail("Wrong input accepted as an answer for a cell!");
 	}
+	
+	/**
+	 * Test for the method setInputCell(String).
+	 */
+	@Test
+	public void testSetInputCell4() {
+		// Call the method under test & Check the result
+		if (!game.setInputCell("1 2 3"))
+			fail("Wrong input accepted as an answer for a cell!");
+	}
 }
