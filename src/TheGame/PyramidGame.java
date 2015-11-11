cdf1480ad1b9ff082e13a6c4ed96f35c6f5daafc
Ivar de Lange <ivardelange2003@hotmail.com>
2015-11-11 22:09:19 +0100

    GREEN
    Manual Test of the solution of the game.

--- src/TheGame/PyramidGame.java
+++ src/TheGame/PyramidGame.java
@@ -92,11 +92,11 @@ public class PyramidGame {
 
 	public void createRandomNumbers() {
 		/**
-		 * Creates random numbers from 1 to 50.
-		 * Amount of Numbers depending on the difficult will be added to the array
+		 * Creates random numbers from 1 to 50. Amount of Numbers depending on
+		 * the difficult will be added to the array
 		 */
 		int size = (int) Math.pow(2, difficulty + 1);
-		for(int i = 0; i < size; i++){
+		for (int i = 0; i < size; i++) {
 			Random random = new Random();
 			randomNumbers = random.nextInt(50) + 1;
 			pyramid[i] = randomNumbers;
@@ -104,6 +104,19 @@ public class PyramidGame {
 		}
 	}
 
-	public void completePyramid(int[] is) {		
+	public boolean completePyramid(int[] base) {
+		if (base.length != (int) Math.pow(2, difficulty + 1))
+			return false;
+		for (int i = 0; i < base.length; i++)
+			pyramid[i] = base[i];
+		pyramid[4] = pyramid[0] + pyramid[1];
+		pyramid[5] = pyramid[1] + pyramid[2];
+		pyramid[6] = pyramid[2] + pyramid[3];
+		pyramid[7] = pyramid[4] + pyramid[5];
+		pyramid[8] = pyramid[5] + pyramid[6];
+		pyramid[9] = pyramid[7] + pyramid[8];
+		for (int i = 0; i < pyramid.length; i++)
+			System.out.print(pyramid[i] + " ");
+		return true;
 	}
 }
