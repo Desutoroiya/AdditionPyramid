ee434e795fa42ae162ccdaf6ebd853af3ba260dc
erasmia <erasmia_10@hotmail.com>
2015-11-11 20:33:26 +0100

    REFACTOR
    Refactored code : setInputCell("17 2 3")

--- src/TheGame/PyramidGame.java
+++ src/TheGame/PyramidGame.java
@@ -61,16 +61,13 @@ public class PyramidGame {
 				System.out.println("Wrong input form of \"" + line + "\".");
 				return false;
 			}
-			x = (int) Integer.parseInt(line.substring(0, i));
-			if (x < 1 || x > (int) Math.pow(2, difficulty + 1)) {
-				System.out.println("Wrong input for cell number");
-				return false;
-			}
 			for (j = i + 1; j < line.length(); j++)
 				if (line.charAt(j) == ' ')
 					break;
+			x = (int) Integer.parseInt(line.substring(0, i));
 			y = (int) Integer.parseInt(line.substring(i + 1, j));
-			if (y < 1 || y > (int) Math.pow(2, difficulty + 1)) {
+			int max = (int) Math.pow(2, difficulty + 1);
+			if (x < 1 || y < 1 || x > max || y > max) {
 				System.out.println("Wrong input for cell number");
 				return false;
 			}
