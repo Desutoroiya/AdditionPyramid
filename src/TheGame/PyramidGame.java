54ae571f4a01d9941ac46703dfc895df0f8b32f0
Ivar de Lange <ivardelange2003@hotmail.com>
2015-11-12 12:17:35 +0100

    GREEN
    Green test for the revised - new completePyramid() method.

--- src/TheGame/PyramidGame.java
+++ src/TheGame/PyramidGame.java
@@ -8,7 +8,6 @@ public class PyramidGame {
 	private int difficulty;
 	int[] pyramid;
 	boolean[] userArray;
-	public int randomNumbers;
 
 	Scanner scanner = new Scanner(System.in);
 
@@ -90,21 +89,28 @@ public class PyramidGame {
 		return userArray;
 	}
 	
-	public void completePyramid() {
-	}
-
-	public void createRandomNumbers() {
-		/**
-		 * Creates random numbers from 1 to 50. Amount of Numbers depending on
-		 * the difficult will be added to the array
-		 */
+	public void completePyramid(){
+		int[] base = createRandomNumbers();
 		int size = (int) Math.pow(2, difficulty + 1);
 		for (int i = 0; i < size; i++) {
-			Random random = new Random();
-			randomNumbers = random.nextInt(50) + 1;
-			pyramid[i] = randomNumbers;
+			pyramid[i] = base[i];
 			userArray[i] = true;
 		}
+		completePyramid(base);
+	}
+	
+	/**
+	 * Creates random numbers from 1 to 50. Amount of Numbers depending on
+	 * the difficult will be added to the array
+	 */
+	public int[] createRandomNumbers() {
+		int size = (int) Math.pow(2, difficulty + 1);
+		int[] randomNumbers = new int[size];
+		for (int i = 0; i < size; i++) {
+			Random random = new Random();
+			randomNumbers[i] = random.nextInt(50) + 1;
+		}
+		return randomNumbers;
 	}
 
 	public boolean completePyramid(int[] base) {
